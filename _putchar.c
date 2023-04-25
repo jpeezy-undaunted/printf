2eb4d1f9b41c7d17185884075aa4be81edeeb076
Paul James <jamespaulc123@gmail.com>
2023-04-25 16:29:58 -0700

    _putchar.c

---
+++ _putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _putchar - Prints character
+ * @character: the character to be printed
+ * Return: 1, success, -1 error
+ */
+
+int _putchar(char character)
+{
+	return(write(1, &character));
+}
