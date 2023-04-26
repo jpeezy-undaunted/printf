4c38b78e73baea60c143c147c1e1b2493e0ebfca
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-04-25 20:32:01 -0700

    printf_string.c added

---
+++ printf_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+int printf_string(va_list args, int display)
+{
+	int i = 0;
+	char *buff;
+
+	buff = va_arg(args, char*);
+	if (buff)
+	{
+		while (buff[i])
+		{
+			_putchar(buff[i]);
+			i++;
+		}
+	}
+	return (i - 1);
+}
