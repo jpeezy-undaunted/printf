b8f1dc17f6dd5e00868e7e3b855e5eec18c08e23
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-04-25 20:08:40 -0700

    selector.c modified: cases added

--- selector.c
+++ selector.c
@@ -9,6 +9,15 @@ int selector(const char *format, va_list args)
 	case 'd':
 		tmp = printf_integer(args, 0);
 		break;
+	case 'i':
+		tmp = printf_integer(args, 0);
+		break;
+	case 'c':
+		tmp = printf_char(args, 0);
+		break;
+	case 's':
+		tmp = printf_string(args, 0);
+		break;
 	default:
 		_putchar(format[i]);
 		tmp++;
